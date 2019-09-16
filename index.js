0142589f07940ece01ffc825933adc49d15f205f
Your Name <you@example.com>
2019-09-16 09:45:00 -0700

    tried hashing but not sure it's correct

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
+const bcrypt = require('bcryptjs')
 
 const db = require('./database/dbConfig.js');
 const Users = require('./users/users-model.js');
@@ -52,5 +53,16 @@ server.get('/api/users', (req, res) => {
     .catch(err => res.send(err));
 });
 
+server.get('hash', (req,res)=> {
+  const user = req.query;
+  //hash the name
+  const credentials = req.body
+  const hash = bcrypt.hashSync(credentials.name, 14);
+  credentials.name = hash;
+  if (!user || !bcrypt.compareSync(credentials.name, user.name)){
+    return res.status(401).json({error: 'Incorrect credentials'})
+  }
+  
+})
 const port = process.env.PORT || 5000;
 server.listen(port, () => console.log(`\n** Running on port ${port} **\n`));
