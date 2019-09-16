2ffb8fbb91059fcf18089cdf1ac6fadcc4a088f4
Your Name <you@example.com>
2019-09-16 10:10:00 -0700

    done generating hash for hash and register, verifying by hash through login

--- index.js
+++ index.js
@@ -17,9 +17,10 @@ server.get('/', (req, res) => {
 });
 
 server.post('/api/register', (req, res) => {
-  let user = req.body;
+  let {username, password} = req.body;
+  const hash = bcrypt.hashSync(password,14)
 
-  Users.add(user)
+  Users.add({username, password: hash})
     .then(saved => {
       res.status(201).json(saved);
     })
@@ -34,7 +35,7 @@ server.post('/api/login', (req, res) => {
   Users.findBy({ username })
     .first()
     .then(user => {
-      if (user) {
+      if (user && bcrypt.compareSync(password, user.password)) {
         res.status(200).json({ message: `Welcome ${user.username}!` });
       } else {
         res.status(401).json({ message: 'Invalid Credentials' });
@@ -53,15 +54,11 @@ server.get('/api/users', (req, res) => {
     .catch(err => res.send(err));
 });
 
-server.get('hash', (req,res)=> {
-  const user = req.query;
+server.get('/hash', (req,res)=> {
+  const name = req.query.name;
   //hash the name
-  const credentials = req.body
-  const hash = bcrypt.hashSync(credentials.name, 14);
-  credentials.name = hash;
-  if (!user || !bcrypt.compareSync(credentials.name, user.name)){
-    return res.status(401).json({error: 'Incorrect credentials'})
-  }
+  const hash = bcrypt.hashSync(name, 10);
+  res.send(`the hash for ${name} is ${hash}`);
   
 })
 const port = process.env.PORT || 5000;
