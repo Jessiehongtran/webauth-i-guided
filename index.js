16a6347e11d0cd979c0e465991a61c1f0313afd1
Your Name <you@example.com>
2019-09-16 10:54:47 -0700

    created middlewaee to validate  user and access to GET api/users

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ server.get('/', (req, res) => {
 
 server.post('/api/register', (req, res) => {
   let {username, password} = req.body;
-  const hash = bcrypt.hashSync(password,14)
+  const hash = bcrypt.hashSync(password,14) // it's 2^14 rounds
 
   Users.add({username, password: hash})
     .then(saved => {
@@ -46,7 +46,20 @@ server.post('/api/login', (req, res) => {
     });
 });
 
-server.get('/api/users', (req, res) => {
+//write a middleware that will check for the username and password  
+//and let the request continue to /api/users if credentials are good  
+//return a 401 if the credentials are invalid
+
+//Use the middleware to restrict access to the GET /api/users endpoint
+function validateUser(req,res,next,username,password){
+  if (username !==req.body.username || password !== req.body.password){
+    res.status(401).json({message: "invalid username or password"})
+  } else if (req.body.name){
+    next()
+  }
+}
+
+server.get('/api/users', validateUser, (req, res) => {
   Users.find()
     .then(users => {
       res.json(users);
@@ -61,5 +74,11 @@ server.get('/hash', (req,res)=> {
   res.send(`the hash for ${name} is ${hash}`);
   
 })
+
+
+
+
 const port = process.env.PORT || 5000;
 server.listen(port, () => console.log(`\n** Running on port ${port} **\n`));
+
+
